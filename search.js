bc3f87f6ba12e276778d4e995f220241dd482b70
Bgillgrist <Bgillgrist@aol.com>
2024-12-19 01:12:17 -0500

    Update search.js

--- search.js
+++ search.js
@@ -10,6 +10,7 @@ const familyMembers = [
   { name: "Frances Lucy Kinsel", link: "frances1937/index.html" },
   { name: "Robert Rob Gillgrist", link: "robert1969/index.html" },
   { name: "Melanie Gillgrist", link: "melanie1967/index.html" },
+  { name: "Samuel Gillgrist", link: "samuel2007/index.html" },
   // Add more family members here
 ];
 
